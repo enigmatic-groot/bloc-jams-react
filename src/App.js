51c1c7c9913e7a02f62eaf2d180e8b68b599f4ab
Jeremiah Judkins <jeremiahjudkins@Jeremiah-Judkinss-iMac.local>
2018-04-12 18:19:34 -0700

    assignment---10----styles

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import "./App.css";
 import Landing from "./components/Landing";
 import Library from "./components/Library";
 import Album from "./components/Album";
-<<<<<<< HEAD
-=======
-
->>>>>>> assignment-10-styling
 class App extends Component {
   render() {
     return (
